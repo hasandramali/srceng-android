1336bc5d1f112fb8f876cf400fd7a468758a3793
hasandramali <93338836+hasandramali@users.noreply.github.com>
2023-01-12 10:08:44 +0200

    Update DirchActivity.java

--- src/me/nillerusr/DirchActivity.java
+++ src/me/nillerusr/DirchActivity.java
@@ -27,7 +27,7 @@ import java.io.InputStream;
 import java.util.regex.Pattern;
 import org.libsdl.app.SDLActivity;
 import android.content.pm.PackageManager;
-import com.valvesoftware.source.R;
+import com.valvesoftware.source32.R;
 import android.widget.LinearLayout.LayoutParams;
 import android.content.pm.PackageInfo;
 import android.content.pm.Signature;
